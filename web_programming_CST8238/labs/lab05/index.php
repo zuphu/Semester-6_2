7eb309a72f2db1f51700805d2446351288862810
anthony <anthony.guev@gmail.com>
2012-09-13 20:47:29 -0700

    lab05 web programming

---
+++ web_programming_CST8238/labs/lab05/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+
+
+<body>
+	<?php
+		echo "First Name: Anthony <br />";
+		echo "Last Name:  Guevara <br />";
+		echo "Student Number: 040576821 <br /><br />";
+		echo "Hello World! I am using PHP! (Well, the echo function anyway) <br />";
+		echo "This course has been an ejoyable experience and has taught me the basics of web development and page layouts. I haven't invested in the course text books but I have found the tutorials on <a href='http://www.w3schools.com'>W3Schools</a> to be particularly helpful. I'm looking forward to starting the PHP unit and hope to learn more about developing for the world wide web.<br />";
+		echo "<br /><ul><li><a href='../Lab1'>Lab1</a></li>";
+		echo "<li><a href='../Lab2'/index.html>Lab2</li>";
+		echo "<li><a href='../Lab3'>Lab3</li>";
+		echo "<li><a href='../Lab4'>Lab4</li></ul>";
+	?>
+</body>
+
+</head>
+</html>
