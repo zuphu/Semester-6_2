0494bec478c36096582a8820299d627a9c864c16
anthony <anthony.guev@gmail.com>
2012-09-14 18:24:39 -0700

    interfacing lab03

--- interfacing_CST8227/labs/lab03/Skype_lnk/Skype_lnk/Skype_lnk.ino
+++ interfacing_CST8227/labs/lab03/Skype_lnk/Skype_lnk/Skype_lnk.ino
@@ -4,6 +4,11 @@
    This example code is in the public domain.
 */
 
+#define RED   0
+#define BLUE  1
+#define GREEN 2
+#define TRI   3
+
 const int redPin   =  0;
 const int bluePin  =  1;
 const int greenPin =  2;
@@ -19,6 +24,7 @@ const int butt = 21; //F0
 // The setup() method runs once, when the sketch starts
 
 void setup()   {
+
   // initialize the digitals pin as an outputs
   pinMode(redPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
@@ -27,15 +33,24 @@ void setup()   {
   pinMode(pwmBluePin, OUTPUT);
   pinMode(pwmGreenPin, OUTPUT);
   pinMode(butt, INPUT);
+
 }
 
 // the loop() method runs over and over again,
 
 void loop()                     
 {
+
+  show_pcm();
+  //green_blue_red_cycle();
+  
+}
+
+void green_blue_red_cycle()
+{
   int state = digitalRead(butt);
   int rand;
-  //show_pcm();
+  
   digitalWrite(redPin, LOW);
   digitalWrite(bluePin, LOW);
   digitalWrite(greenPin, LOW);
@@ -117,32 +132,67 @@ void loop()
 void show_pcm()
 {
   int i;
+  static int colour_intensity = 0;
+  static int current_colour = RED;
+  static int state = LOW;
   
-  for (i = 0; i < 255; ++i)
+
+  if (firstPress == LOW)
   {
-    analogWrite(pwmRedPin, i);
-    delay(5);
+    if ((state = digitalRead(butt)) == HIGH)
+    {
+      firstPress = HIGH;
+      while ((state = digitalRead(butt)) == HIGH)
+        ;
+    }
+    
+    switch (current_colour)
+    {
+      case RED:
+        analogWrite(pwmRedPin, colour_intensity);
+        break;
+      case BLUE:
+        analogWrite(pwmBluePin, colour_intensity);    
+        break;
+      case GREEN:
+        analogWrite(pwmGreenPin, colour_intensity); 
+        break;
+      default:
+         analogWrite(pwmRedPin, colour_intensity);
+         analogWrite(pwmBluePin, colour_intensity); 
+         analogWrite(pwmGreenPin, colour_intensity); 
+    }
+    
+    if (colour_intensity == 255)
+    {
+      current_colour = (current_colour < 3) ? ++current_colour: 0;
+      reset_colours();
+    }
+    colour_intensity = (colour_intensity < 255) ? ++colour_intensity : 0;
   }
-  digitalWrite(pwmRedPin, LOW);
-  for (i = 0; i < 255; ++i)
+  else
   {
-    analogWrite(pwmBluePin, i);
-    delay(5);
+    if ((state = digitalRead(butt)) == HIGH)
+    {
+      randomize();
+      while ((state = digitalRead(butt)) == HIGH)
+        ;
+    }
   }
-  digitalWrite(pwmBluePin, LOW);
-  for (i = 0; i < 255; ++i)
-  {
-    analogWrite(pwmGreenPin, i);
-    delay(5);
-  }
-  digitalWrite(pwmGreenPin, LOW);
-  
-  for (i = 0; i < 255; ++i)
-  {
-    analogWrite(pwmRedPin,   i);
-    analogWrite(pwmBluePin,  i);
-    analogWrite(pwmGreenPin, i);
-    delay(5);
-  }
+  delay(5);
+}
+
+void randomize()
+{
+   analogWrite(pwmRedPin, random(0, 256));
+   analogWrite(pwmBluePin, random(0, 256)); 
+   analogWrite(pwmGreenPin, random(0, 256)); 
+}
+
+void reset_colours()
+{
+  analogWrite(pwmRedPin, 0);
+  analogWrite(pwmBluePin, 0);
+  analogWrite(pwmGreenPin, 0);
 }
 
