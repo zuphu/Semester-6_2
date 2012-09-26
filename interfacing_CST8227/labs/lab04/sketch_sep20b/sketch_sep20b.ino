eeaccbf7a407d650a566c5170db0b9660314abfd
ant guv <anthony.guev@gmail.com>
2012-09-26 15:43:56 -0400

    lab 04 interfacing

--- interfacing_CST8227/labs/lab04/sketch_sep20b/sketch_sep20b.ino
+++ interfacing_CST8227/labs/lab04/sketch_sep20b/sketch_sep20b.ino
@@ -1,4 +1,4 @@
-byte seven_seg_digits[10][8] = { { 0,0,0,0,0,0,1,1},  // = 0
+byte seven_seg_digits[16][8] = { { 0,0,0,0,0,0,1,1},   // = 0
                                  { 1,0,0,1,1,1,1,1 },  // = 1
                                  { 0,0,1,0,0,1,0,1 },  // = 2
                                  { 0,0,0,0,1,1,0,1 },  // = 3
@@ -7,17 +7,26 @@ byte seven_seg_digits[10][8] = { { 0,0,0,0,0,0,1,1},  // = 0
                                  { 0,1,0,0,0,0,0,1 },  // = 6
                                  { 0,0,0,1,1,1,1,1 },  // = 7
                                  { 0,0,0,0,0,0,0,1 },  // = 8
-                                 { 0,0,0,0,1,0,0,1 }   // = 9
+                                 { 0,0,0,0,1,0,0,1 },  // = 9
+                                 { 0,0,0,1,0,0,0,1 },  // A
+                                 { 1,1,0,0,0,0,0,1 },  // B
+                                 { 1,1,1,0,0,1,0,1 },  // C
+                                 { 1,0,0,0,0,1,0,1 },  // D
+                                 { 0,1,1,0,0,0,0,1 },  // E
+                                 { 0,1,1,1,0,0,0,1 }   // F
                                  };
 int pwmPins[] = {4, 5, 9, 10, 12, 14, 15};
+
+#define DISPLAY2 21
+#define DISPLAY1 20
 
 void
 setup()
 {
-  pinMode(21, OUTPUT);
-  digitalWrite(21, HIGH );
-    pinMode(20, OUTPUT);
-  digitalWrite(20, LOW );
+  pinMode(DISPLAY1, OUTPUT);
+  digitalWrite(DISPLAY1, HIGH );
+  pinMode(DISPLAY2, OUTPUT);
+  digitalWrite(DISPLAY2, HIGH );
   
   pinMode(0, OUTPUT);
   pinMode(1, OUTPUT);
@@ -40,24 +49,30 @@ setup()
 void
 loop()
 {
+  static int first_digit = 0;
   static int iii = -1;
   
   for (int i = 0; i < 8; ++i)
     digitalWrite(pwmPins[i], HIGH);
   
-  if (iii < 9)
+  if (iii < 15)
     ++iii;
   else
     iii = 0;
-  
-  for (int i = 0; i < 8; ++i)
-    digitalWrite(i, HIGH);
 
-  for (int pin = 0; pin < 8; ++pin)
+  for (int pin = 0; pin < 9; ++pin)
   {
     if (seven_seg_digits[iii][pin] == 0)
       analogWrite(pwmPins[pin], 31.875);
   }
-      delay(500);
+  
+  delay(500);
+
+}
+
+void clear()
+{  
+  for (int i = 0; i < 9; ++i)
+    digitalWrite(i, HIGH);
 }
 
